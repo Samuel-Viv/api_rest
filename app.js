a0ae9a6b3eb4a22f146356e332daf867fdfa2230
Samuel VIVIER <samvivier25@gmail.com>
2024-06-14 18:50:13 +0200

    add modif

--- app.js
+++ app.js
@@ -8,12 +8,13 @@ On ajoute un retour en cas de requête sur une route inexistante (404).
  */
 
 var express = require('express');
-
+var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const cors = require('cors');
 
 var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 const mongodb = require('./db/mongo.js'); 
 
 mongodb.initClientDbConnection();
@@ -28,8 +29,10 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 app.use(function(req, res, next) {
     res.status(404).json({name:'API', version:"1.0", status: 404, message: 'not_found'})
